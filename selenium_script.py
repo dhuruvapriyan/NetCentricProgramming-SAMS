a3e8eaf84ea82c16e9e83e30ecd6b80277241f6a
Dhuruva Priyan G M <gmdhuruva@gmail.com>
2019-10-15 18:09:05 +0530

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re,os
+import unittest, time, re ,os
 
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
@@ -21,13 +21,14 @@ class AppDynamicsJob(unittest.TestCase):
     def test_app_dynamics_job(self):
         driver = self.driver
         driver.get("http://localhost:9999/sams/index.jsp")
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Student Attendance Management System'])[1]/following::a[1]").click()
         driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[1]").click()
         driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[1]").clear()
         driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[1]").send_keys("t001")
         driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[2]").clear()
         driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[2]").send_keys("789456123")
         driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Forgot Password'])[2]/following::button[1]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Brand'])[1]/preceding::i[1]").click()
+        driver.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
