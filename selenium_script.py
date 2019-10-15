2d85e2d1848f5faa00040cd0569652b0c2429311
Dhuruva Priyan G M <gmdhuruva@gmail.com>
2019-10-15 17:52:57 +0530

    Add files via upload

---
+++ selenium_script.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re,os
+
+class AppDynamicsJob(unittest.TestCase):
+    def setUp(self):
+        # AppDynamics will automatically override this web driver
+        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
+        os.environ["webdriver.chrome.driver"] = "C:\\tomcat\\webapps\\sams"
+        self.driver = webdriver.Chrome();
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.katalon.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_app_dynamics_job(self):
+        driver = self.driver
+        driver.get("http://localhost:9999/sams/index.jsp")
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Student Attendance Management System'])[1]/following::a[1]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[1]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[1]").clear()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[1]").send_keys("t001")
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[2]").clear()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Teacher Login'])[1]/following::input[2]").send_keys("789456123")
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Forgot Password'])[2]/following::button[1]").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        # To know more about the difference between verify and assert,
+        # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
